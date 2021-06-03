c463cd5328b59dca3898c69a3ceceeb1e0a4de2f
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-03 23:53:39 +0530

    Creating Layout

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,26 @@
-import Head from "next/head";
-import Image from "next/image";
+import { useState } from "react";
 import CoinList from "../components/CoinList";
-import Coins from "../components/Coins";
+import Layout from "../components/Layout";
 import SearchBar from "../components/SearchBar";
 
 export default function Home({ filteredCoins }) {
+  const [search, setSearch] = useState("");
+  const allCoins = filteredCoins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setSearch(e.target.value.toLowerCase());
+  };
   return (
-    <div>
-      <Head>
-        <title>Kunver Next App </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <SearchBar type="text" placeholder="Search" />
-      <CoinList filteredCoins={filteredCoins} />
-    </div>
+    <Layout>
+      <div className="coin_app">
+        <SearchBar type="text" placeholder="Search" onChange={handleChange} />
+        <CoinList filteredCoins={allCoins} />
+      </div>
+    </Layout>
   );
 }
 
