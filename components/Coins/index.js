b97bffc073f5d5636b53f2042e125090b9877277
Kunver Siddharth <kunversiddharth@outlook.com>
2021-06-04 00:11:22 +0530

    Creating Dynamic Page

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,4 +1,5 @@
 import styles from "./Coins.module.css";
+import Link from "next/link";
 const Coins = ({
   name,
   price,
@@ -10,33 +11,39 @@ const Coins = ({
   id,
 }) => {
   return (
-    <div className={styles.coin_container}>
-      <div className={styles.coin_row}>
-        <div className={styles.coin}>
-          <img src={image} alt={name} className={styles.coin_img} />
-          <h1 className={styles.coin_h1}>{name}</h1>
-          <p className={styles.coin_symbol}>{symbol}</p>
-        </div>
-        <div className={styles.coin_data}>
-          <p className={styles.coin_price}>&#8377;{price}</p>
-          <p className={styles.coin_volume}>&#8377;{volume.toLocaleString()}</p>
+    <Link href="/coin/[id]" as={`/coin/${id}`}>
+      <a>
+        <div className={styles.coin_container}>
+          <div className={styles.coin_row}>
+            <div className={styles.coin}>
+              <img src={image} alt={name} className={styles.coin_img} />
+              <h1 className={styles.coin_h1}>{name}</h1>
+              <p className={styles.coin_symbol}>{symbol}</p>
+            </div>
+            <div className={styles.coin_data}>
+              <p className={styles.coin_price}>&#8377;{price}</p>
+              <p className={styles.coin_volume}>
+                &#8377;{volume.toLocaleString()}
+              </p>
 
-          {priceChange < 0 ? (
-            <p className={(styles.coin_percent, styles.red)}>
-              {priceChange.toFixed(2)}%
-            </p>
-          ) : (
-            <p className={(styles.coin_percent, styles.green)}>
-              {priceChange.toFixed(2)}%
-            </p>
-          )}
+              {priceChange < 0 ? (
+                <p className={(styles.coin_percent, styles.red)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              ) : (
+                <p className={(styles.coin_percent, styles.green)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              )}
 
-          <p className={styles.coin_marketcap}>
-            Mkt Cap: &#8377;{marketcap.toLocaleString()}
-          </p>
+              <p className={styles.coin_marketcap}>
+                Mkt Cap: &#8377;{marketcap.toLocaleString()}
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </a>
+    </Link>
   );
 };
 
